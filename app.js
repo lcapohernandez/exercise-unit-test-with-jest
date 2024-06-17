c731080176835168cf5dbd0f68e2646e81eb61c8
Laura Irene Capo <144149179+lcapohernandez@users.noreply.github.com>
2024-06-17 23:19:38 +0000

    "first commit"

---
+++ app.js
@@ -0,0 +1,22 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+const fromDollartoYen = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+const sum = (a,b) => {
+    return a + b
+}
+console.log(sum(7,3))
+
+
+
+// We include fromEuroToDollar here as well because it needs to be exported
+module.exports = { sum, fromEuroToDollar }
